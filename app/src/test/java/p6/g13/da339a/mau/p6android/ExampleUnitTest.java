e5ae659daedc19e0b6b02d716f41f7fd1fe26315
Pontus <laos.pontus@gmail.com>
2019-01-14 11:31:08 +0100

    This almost certainly does not build, committing for reference.

---
+++ app/src/test/java/p6/g13/da339a/mau/p6android/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package p6.g13.da339a.mau.p6android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
